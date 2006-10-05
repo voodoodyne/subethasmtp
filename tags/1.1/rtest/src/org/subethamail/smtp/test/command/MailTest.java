87d450d48ec54ea504c036d15176556e91551fe7
jon <jon@d4f6eda6-38e6-11de-8fef-1f5ae417a2df>
2006-10-05 23:53:25 +0000

    release of 1.1

---
+++ tags/1.1/rtest/src/org/subethamail/smtp/test/command/MailTest.java
@@ -0,0 +1,69 @@
+package org.subethamail.smtp.test.command;
+
+import org.subethamail.smtp.test.ServerTestCase;
+
+/**
+ * @author Jon Stevens
+ */
+public class MailTest extends ServerTestCase
+{
+	public MailTest(String name)
+	{
+		super(name);
+	}
+
+	public void testMailNoHello() throws Exception
+	{
+		expect("220");
+
+		send("MAIL FROM: test@example.com");
+		expect("503 Error: send HELO/EHLO first");
+	}
+	
+	public void testAlreadySpecified() throws Exception
+	{
+		expect("220");
+
+		send("HELO foo.com");
+		expect("250");
+
+		send("MAIL FROM: test@example.com");
+		expect("250 Ok");
+
+		send("MAIL FROM: another@example.com");
+		expect("503 Sender already specified.");
+	}
+
+	public void testInvalidSenders() throws Exception
+	{
+		expect("220");
+
+		send("HELO foo.com");
+		expect("250");
+
+		send("MAIL FROM: test@lkjsd lkjk");
+		expect("553 <test@lkjsd lkjk> Invalid email address.");
+	}
+
+	public void testMalformedMailCommand() throws Exception
+	{
+		expect("220");
+
+		send("HELO foo.com");
+		expect("250");
+
+		send("MAIL");
+		expect("501 Syntax: MAIL FROM: <address>  Error in parameters:");
+	}
+
+	public void testMailWithoutWhitespace() throws Exception
+	{
+		expect("220");
+
+		send("HELO foo.com");
+		expect("250");
+
+		send("MAIL FROM:<validuser@subethamail.org>");
+		expect("250 Ok");
+	}
+}
