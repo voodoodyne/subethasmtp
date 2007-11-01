7d9e29e53e17cf22d7d5232ffc35ab199d1c0154
jon <jon@d4f6eda6-38e6-11de-8fef-1f5ae417a2df>
2007-11-01 15:05:55 +0000

    code formatting

--- smtp/src/org/subethamail/smtp/server/io/ByteBufferInputStream.java
+++ smtp/src/org/subethamail/smtp/server/io/ByteBufferInputStream.java
@@ -5,179 +5,181 @@ import java.io.InputStream;
 
 import org.apache.mina.common.ByteBuffer;
 
-public class ByteBufferInputStream 
-    extends InputStream
+/**
+ * Wrapper around InputStream to convert data into a ByteBuffer. 
+ */
+public class ByteBufferInputStream extends InputStream
 {
-    private final Object mutex = new Object();
-    private final ByteBuffer buf;
+	private final Object mutex = new Object();
+	private final ByteBuffer buf;
+	private volatile boolean closed;
+	private volatile boolean released;
+	private IOException exception;
 
-    private volatile boolean closed;
-    private volatile boolean released;
-    private IOException exception;
+	public ByteBufferInputStream()
+	{
+		buf = ByteBuffer.allocate(16);
+		buf.setAutoExpand(true);
+		buf.limit(0);
+	}
 
-    public ByteBufferInputStream()
-    {
-        buf = ByteBuffer.allocate( 16 );
-        buf.setAutoExpand( true );
-        buf.limit( 0 );
-    }
+	public int available()
+	{
+		if (released)
+		{
+			return 0;
+		}
+		else
+		{
+			synchronized (mutex)
+			{
+				return buf.remaining();
+			}
+		}
+	}
 
-    public int available()
-    {
-        if( released )
-        {
-            return 0;
-        }
-        else
-        {
-            synchronized( mutex )
-            {
-                return buf.remaining();
-            }
-        }
-    }
+	public void close()
+	{
+		if (closed)
+		{
+			return;
+		}
 
-    public void close()
-    {
-        if( closed )
-        {
-            return;
-        }
+		synchronized (mutex)
+		{
+			closed = true;
+			releaseBuffer();
 
-        synchronized( mutex )
-        {
-            closed = true;
-            releaseBuffer();
+			mutex.notifyAll();
+		}
+	}
 
-            mutex.notifyAll();
-        }
-    }
+	public int read() throws IOException
+	{
+		synchronized (mutex)
+		{
+			if (!waitForData())
+			{
+				return -1;
+			}
 
-    public int read() throws IOException
-    {
-        synchronized( mutex )
-        {
-            if( !waitForData() )
-            {
-                return -1;
-            }
+			return buf.get() & 0xff;
+		}
+	}
 
-            return buf.get() & 0xff;
-        }
-    }
+	public int read(byte[] b, int off, int len) throws IOException
+	{
+		synchronized (mutex)
+		{
+			if (!waitForData())
+			{
+				return -1;
+			}
 
-    public int read( byte[] b, int off, int len ) throws IOException
-    {
-        synchronized( mutex )
-        {
-            if( !waitForData() )
-            {
-                return -1;
-            }
+			int readBytes;
 
-            int readBytes;
+			if (len > buf.remaining())
+			{
+				readBytes = buf.remaining();
+			}
+			else
+			{
+				readBytes = len;
+			}
 
-            if( len > buf.remaining() )
-            {
-                readBytes = buf.remaining();
-            }
-            else
-            {
-                readBytes = len;
-            }
+			buf.get(b, off, readBytes);
 
-            buf.get( b, off, readBytes );
+			return readBytes;
+		}
+	}
 
-            return readBytes;
-        }
-    }
+	private boolean waitForData() throws IOException
+	{
+		if (released)
+		{
+			return false;
+		}
 
-    private boolean waitForData() throws IOException
-    {
-        if( released )
-        {
-            return false;
-        }
+		synchronized (mutex)
+		{
+			while (!released && buf.remaining() == 0 && exception == null)
+			{
+				try
+				{
+					mutex.wait();
+				}
+				catch (InterruptedException e)
+				{
+					IOException ioe = new IOException(
+							"Interrupted while waiting for more data");
+					ioe.initCause(e);
+					throw ioe;
+				}
+			}
+		}
 
-        synchronized( mutex )
-        {
-            while( !released && buf.remaining() == 0 && exception == null )
-            {
-                try
-                {
-                    mutex.wait();
-                }
-                catch( InterruptedException e )
-                {
-                    IOException ioe = new IOException( "Interrupted while waiting for more data" );
-                    ioe.initCause( e );
-                    throw ioe;
-                }
-            }
-        }
+		if (exception != null)
+		{
+			releaseBuffer();
+			throw exception;
+		}
 
-        if( exception != null )
-        {
-            releaseBuffer();
-            throw exception;
-        }
+		if (closed && buf.remaining() == 0)
+		{
+			releaseBuffer();
 
-        if( closed && buf.remaining() == 0 )
-        {
-            releaseBuffer();
+			return false;
+		}
 
-            return false;
-        }
+		return true;
+	}
 
-        return true;
-    }
+	private void releaseBuffer()
+	{
+		if (released)
+		{
+			return;
+		}
 
-    private void releaseBuffer()
-    {
-        if( released )
-        {
-            return;
-        }
+		released = true;
+		buf.release();
+	}
 
-        released = true;
-        buf.release();
-    }
+	public void write(byte[] src)
+	{
+		synchronized (mutex)
+		{
+			if (closed)
+			{
+				return;
+			}
 
-    public void write(byte[] src)
-    {
-        synchronized( mutex )
-        {
-            if( closed )
-            {
-                return;
-            }
+			if (buf.hasRemaining())
+			{
+				this.buf.compact();
+				this.buf.put(src);
+				this.buf.flip();
+			}
+			else
+			{
+				this.buf.clear();
+				this.buf.put(src);
+				this.buf.flip();
+				mutex.notifyAll();
+			}
+		}
+	}
 
-            if( buf.hasRemaining() )
-            {
-                this.buf.compact();
-                this.buf.put( src );
-                this.buf.flip();
-            }
-            else
-            {
-                this.buf.clear();
-                this.buf.put( src );
-                this.buf.flip();
-                mutex.notifyAll();
-            }
-        }
-    }
+	public void throwException(IOException e)
+	{
+		synchronized (mutex)
+		{
+			if (exception == null)
+			{
+				exception = e;
 
-    public void throwException( IOException e )
-    {
-        synchronized( mutex )
-        {
-            if( exception == null )
-            {
-                exception = e;
-
-                mutex.notifyAll();
-            }
-        }
-    }
+				mutex.notifyAll();
+			}
+		}
+	}
 }
