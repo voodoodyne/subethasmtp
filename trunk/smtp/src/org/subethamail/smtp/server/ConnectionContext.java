be058ff4368ffdaf868e3491df7a69e30dd58601
ted_kods <ted_kods@d4f6eda6-38e6-11de-8fef-1f5ae417a2df>
2008-01-08 01:05:25 +0000

    Added ability to get credential informations

--- trunk/smtp/src/org/subethamail/smtp/server/ConnectionContext.java
+++ trunk/smtp/src/org/subethamail/smtp/server/ConnectionContext.java
@@ -1,23 +1,21 @@
 package org.subethamail.smtp.server;
 
 import java.io.IOException;
-import java.net.SocketAddress;
 
 import org.apache.mina.common.IoSession;
-import org.subethamail.smtp.server.io.SMTPMessageDataStream;
+import org.subethamail.smtp.auth.Credential;
 
 /**
  * This context is used for managing information
  * about a connection.
  * 
  * @author Jon Stevens
+ * @author De Oliveira Edouard &lt;doe_wanted@yahoo.fr&gt;
  */
-public interface ConnectionContext
+public interface ConnectionContext extends BaseContext
 {
-	public SMTPMessageDataStream getInput();
+	public IoSession getIOSession();	
 	public Session getSession();
 	public void sendResponse(String response) throws IOException;
-	public SocketAddress getRemoteAddress();
-	public SMTPServer getSMTPServer();
-	public IoSession getIOSession();
+	public void setCredential(Credential cred);
 }
