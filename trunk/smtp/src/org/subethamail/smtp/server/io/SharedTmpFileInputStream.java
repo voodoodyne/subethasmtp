0b6ae43e95ac7d60ca7fbc6beec61de4a61a1e0e
ted_kods <ted_kods@d4f6eda6-38e6-11de-8fef-1f5ae417a2df>
2008-02-25 20:37:15 +0000

    Doc updated

--- trunk/smtp/src/org/subethamail/smtp/server/io/SharedTmpFileInputStream.java
+++ trunk/smtp/src/org/subethamail/smtp/server/io/SharedTmpFileInputStream.java
@@ -5,6 +5,12 @@ import java.io.IOException;
 
 import javax.mail.util.SharedFileInputStream;
 
+/**
+ * This class uses a temporary file to store big messages and asks JVM
+ * to delete them when destroyed.
+ * 
+ * @author De Oliveira Edouard &lt;doe_wanted@yahoo.fr&gt;
+ */
 public class SharedTmpFileInputStream 
 	extends SharedFileInputStream 
 {
