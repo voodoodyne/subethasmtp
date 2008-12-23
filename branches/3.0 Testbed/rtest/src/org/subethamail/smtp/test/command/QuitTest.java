101dc3759d2acf1c9d41c365e081668efdf06b75
skot <skot@d4f6eda6-38e6-11de-8fef-1f5ae417a2df>
2008-12-23 21:53:12 +0000

    Working area for 3.0 changes.

---
+++ branches/3.0 Testbed/rtest/src/org/subethamail/smtp/test/command/QuitTest.java
@@ -0,0 +1,28 @@
+package org.subethamail.smtp.test.command;
+
+import org.subethamail.smtp.test.ServerTestCase;
+
+/**
+ * @author Jon Stevens
+ */
+public class QuitTest extends ServerTestCase
+{
+	public QuitTest(String name)
+	{
+		super(name);
+	}
+
+	public void testQuit() throws Exception
+	{
+		expect("220");
+
+		send("HELO foo.com");
+		expect("250");
+
+		send("MAIL FROM: test@example.com");
+		expect("250 Ok");
+
+		send("QUIT");
+		expect("221 Bye");
+	}
+}
