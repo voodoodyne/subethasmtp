d9fc95a438a6b27739687ac231a8f77fbc6b2e1b
lhoriman <lhoriman@d4f6eda6-38e6-11de-8fef-1f5ae417a2df>
2009-05-01 04:01:08 +0000

    Moving v3 branch files into trunk the hard way.

---
+++ trunk/smtp/src/org/subethamail/smtp/util/EmailUtils.java
@@ -0,0 +1,47 @@
+package org.subethamail.smtp.util;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+/**
+ * @author Jeff Schnitzer
+ */
+public class EmailUtils
+{
+	/**
+	 * @return true if the string is a valid email address
+	 */
+	public static boolean isValidEmailAddress(String address)
+	{
+		// MAIL FROM: <>
+		if (address.length() == 0)
+			return true;
+
+		boolean result = false;
+		try
+		{
+			InternetAddress[] ia = InternetAddress.parse(address, true);
+			if (ia.length == 0)
+				result = false;
+			else
+				result = true;
+		}
+		catch (AddressException ae)
+		{
+			result = false;
+		}
+		return result;
+	}
+
+	/**
+	 * Extracts the email address within a <> after a specified offset.
+	 */
+	public static String extractEmailAddress(String args, int offset)
+	{
+		String address = args.substring(offset).trim();
+		if (address.indexOf('<') == 0)
+			address = address.substring(1, address.indexOf('>'));
+		
+		return address;
+	}
+}
