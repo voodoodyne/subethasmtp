cf73d3fb3126f01ebf79905df76b3db5828dd9d3
hontvari.jozsef.levente@gmail.com <hontvari.jozsef.levente@gmail.com@d4f6eda6-38e6-11de-8fef-1f5ae417a2df>
2011-10-10 20:12:55 +0000

    If MultipleAuthenticationHandlerFactory was installed, then AUTH command was only accepted if the client supplied it in upper case. In addition the SASL mechanism name was also accepted case sensitively in this case, which seems to be correct (SASL names are upper case only, this is explicitly specified by SASL), but differs from the AuthCommand itself. I changed it to be case insensitive too, so the behavior is uniform.

--- src/main/java/org/subethamail/smtp/AuthenticationHandlerFactory.java
+++ src/main/java/org/subethamail/smtp/AuthenticationHandlerFactory.java
@@ -14,7 +14,7 @@ public interface AuthenticationHandlerFactory
 	 * If your handler supports RFC 2554 at some degree, then it must return all the supported mechanisms here. <br>
 	 * The order you use to populate the list will be preserved in the output of the EHLO command. <br>
 	 *
-	 * @return the supported authentication mechanisms as List.
+	 * @return the supported authentication mechanisms as List, names are in upper case.
 	 */
 	public List<String> getAuthenticationMechanisms();
 
